adb99a58d4abdb867cb2bf9ee913c959e2cb8253
Lisa Wagner <lisawagner.online@gmail.com>
2021-12-03 19:39:22 -0800

    added camera controls to follow player and respawn point

---
+++ Assets/Scripts/CameraController2.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController2 : MonoBehaviour
+{
+    public GameObject player;
+    [SerializeField] private float offset;
+    [SerializeField] private float offsetSmoothing;
+    private Vector3 playerPosition;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPosition = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+
+        if(player.transform.localScale.x > 0f)
+        {
+            playerPosition = new Vector3(playerPosition.x + offset, playerPosition.y, playerPosition.z);
+        }
+        else
+        {
+            playerPosition = new Vector3(playerPosition.x - offset, playerPosition.y, playerPosition.z);
+        }
+        transform.position = Vector3.Lerp(transform.position, playerPosition, offsetSmoothing * Time.deltaTime);
+    }
+}
--- Assets/Scripts/PlayerController2.cs
+++ Assets/Scripts/PlayerController2.cs
@@ -14,10 +14,18 @@ public class PlayerController2 : MonoBehaviour
     [SerializeField] private LayerMask groundLayer;
     private bool isTouchingGround;
 
+    private Animator playerAnimation;
+    private AudioSource footsteps;
+
+    [SerializeField] private Vector3 respawnPoint;
+    public GameObject fallDetector;
 
     void Start()
     {
         player = GetComponent<Rigidbody2D>();
+        playerAnimation = GetComponent<Animator>();
+        footsteps = GetComponent<AudioSource>();
+        respawnPoint = transform.position; // stores players initial position to respawn to
     }
 
     // Update is called once per frame
@@ -30,13 +38,14 @@ public class PlayerController2 : MonoBehaviour
         if (direction > 0f)
         {
             player.velocity = new Vector2(direction * speed, player.velocity.y);
+            transform.localScale = new Vector2(1, 1);
         }
         else if (direction < 0f)
         {
             // if player is on a tilted platform, player can slide when keys not pressed
             // this keeps the player still when not pressing keys
             player.velocity = new Vector2(direction * speed, player.velocity.y);
-
+            transform.localScale = new Vector2(-1, 1);
         }
         else
         {
@@ -48,5 +57,26 @@ public class PlayerController2 : MonoBehaviour
         {
             player.velocity = new Vector2(player.velocity.x, jumpSpeed);
         }
+
+        // to change speed parameter in animator
+        playerAnimation.SetFloat("Speed", Mathf.Abs(player.velocity.x));
+        playerAnimation.SetBool("OnGround", isTouchingGround);
+
+        // fall detection - follow the player on the x axis
+        fallDetector.transform.position = new Vector2(transform.position.x, fallDetector.transform.transform.position.y);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "FallDetector")
+        {
+            transform.position = respawnPoint;
+        }
+    }
+
+    private void FootSounds()
+    {
+        footsteps.Play();
     }
 }
