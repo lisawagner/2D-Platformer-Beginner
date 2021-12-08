01bd22910e0c00f06afbdbaaee2bbe91585073c7
Lisa Wagner <lisawagner.online@gmail.com>
2021-12-07 19:41:05 -0800

    refactor - added inheritance to enemy so there can be different enemies

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,76 +4,16 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    [Header("Enemy Bounds")]
-    [SerializeField] private float leftCap;
-    [SerializeField] private float rightCap;
 
-    [Header("Enemy Movement")]
-    [SerializeField] private float jumpLength = 2;
-    [SerializeField] private float jumpHeight = 2;
-    [SerializeField] private LayerMask Ground;
+    protected Animator enemyAnimator; /// PROTECTED VARIABLE ///
 
-    private Animator enemyAnimator;
-    private Collider2D enemyHitBox;
-    private Rigidbody2D enemy;
-
-    private bool isFacingLeft = true;
-
-    //TODO: Redo enemyAI to idle/walk/run depending on detecting player nearby
-    void Start()
+    protected virtual void Start()
+    /// PROTECTED ACCESS: Only children can access the animator ///
+    /// VIRTUAL:  ///
     {
-        enemy = GetComponent<Rigidbody2D>();
-        enemyHitBox = GetComponent<Collider2D>();
         enemyAnimator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        EnemyMovementHandler();
-    }
-
-    private void EnemyMovementHandler()
-    {
-        if (isFacingLeft)
-        {
-            if (transform.position.x > leftCap)
-            {
-                if (transform.localScale.x != 1)
-                {
-                    transform.localScale = new Vector3(1, 1, 1);
-                }
-
-                if (enemyHitBox.IsTouchingLayers(Ground))
-                {
-                    enemy.velocity = new Vector2(-jumpLength, jumpHeight);
-                }
-            }
-            else
-            {
-                isFacingLeft = false;
-            }
-        }
-        else
-        {
-            if (transform.position.x < rightCap)
-            {
-                if (transform.localScale.x != -1)
-                {
-                    transform.localScale = new Vector3(-1, 1, 1);
-                }
-
-                if (enemyHitBox.IsTouchingLayers(Ground))
-                {
-                    enemy.velocity = new Vector2(jumpLength, jumpHeight);
-                }
-            }
-            else
-            {
-                isFacingLeft = true;
-            }
-        }
-    }
 
     public void HeadSmashedIn()
     {
