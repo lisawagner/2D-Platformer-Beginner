4472e8b509160bb6cfe57dd63c1484b1db0e49a7
Lisa Wagner <lisawagner.online@gmail.com>
2021-12-11 11:51:24 -0800

    refactored main menu, added transition

--- Assets/Scripts/InterfaceScripts/MainMenu.cs
+++ Assets/Scripts/InterfaceScripts/MainMenu.cs
@@ -2,15 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private Image fadeImage = null;
     public void PlayGame()
     {
+        StartCoroutine(StartTransition());
+        //SceneManager.LoadScene(1);
+    }
+
+    IEnumerator StartTransition(bool fadeToBlack = true, float fadeSpeed = 0.4f)
+    {
+        Color objectColor = fadeImage.color;
+        float fadeAmount;
+
+        while (fadeImage.color.a < 1)
+        {
+            fadeAmount = objectColor.a + (fadeSpeed * Time.deltaTime);
+            objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+            fadeImage.color = objectColor;
+            yield return null;
+        }
+
         SceneManager.LoadScene(1);
     }
+
     public void Exit()
     {
         Debug.Log("Quit");
